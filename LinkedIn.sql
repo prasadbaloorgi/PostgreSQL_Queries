9405e48c258db3a26100e5cda82c50f5398e72bf
prasadbaloorgi <167971067+prasadbaloorgi@users.noreply.github.com>
2024-06-03 10:21:04 +0530

    Update LinkedIn.sql

--- LinkedIn.sql
+++ LinkedIn.sql
@@ -1,5 +1,8 @@
-  /* Given a table of candidates and their skills, you're tasked with finding the candidates best suited for an open Data Science job. You want to find candidates who are proficient in Python, Tableau, and PostgreSQL.
-   Write a query to list the candidates who possess all of the required skills for the job. Sort the output by candidate ID in ascending order
+  /* Given a table of candidates and their skills, you're tasked with finding the candidates 
+     best suited for an open Data Science job. You want to find candidates who are proficient 
+     in Python, Tableau, and PostgreSQL.
+     Write a query to list the candidates who possess all of the required skills for the job. 
+     Sort the output by candidate ID in ascending order
 
   candidates Example Input:
 candidate_id	skill
